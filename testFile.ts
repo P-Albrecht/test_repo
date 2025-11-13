50a4a5c7930b26c7122e83bdf22b3ec84ed5e8d5
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2025-11-13 16:55:38 +0100

    Create testFile.ts

---
+++ testFile.ts
@@ -0,0 +1,26 @@
+import { xxx } from './bridgeTest';
+import { RRR } from './ggg/bridgeTest3';
+
+
+class aaa (a , b , c ) {
+
+    let hhh ;
+    let zzz ;
+
+    function philip1 ( Q1 ) { 
+        let xxx;
+
+        function philip2 ( Q2 ) { 
+
+            //global ttt;
+            global ttt;
+            
+            let xxx;
+        }
+
+        let yyy;
+    }
+    
+}
+
+aaa.philip1;
