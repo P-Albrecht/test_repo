fdf49a51def1defa3b4eb7ab2e10ebfa5297efb5
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2025-11-30 13:51:43 +0100

    Clean up class C3 by removing unused code
    Removed unused variables and function in class C3.

--- refFile.ts
+++ refFile.ts
@@ -1,12 +1,11 @@
 class C3 () {
   // hello world
 
-    let hhh ;
-  
-    function F1 ( hhh ) { 
-        let bbb = hhh;
-    }
-    
-    let ccc = hhh;
+  aaa();
+  {
+    aaa();
+    aaa();
+  }
+  aaa();
     
 }
